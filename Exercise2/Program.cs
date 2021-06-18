9976c36905ac254eb2ea1a0aaa07d14bcb9256f2
NguyenQuangHuy <huynqth2008064@fpt.edu.vn>
2021-06-18 15:35:11 +0700

    update

---
+++ Exercise2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SearchWord();
+        }
+
+        private static void SearchWord()
+        {
+            int count = 0;
+            Console.WriteLine("Enter a String");
+            string srt = Console.ReadLine();
+            Console.WriteLine("Enter a word to search");
+            string word = Console.ReadLine();
+            string[] c = srt.Split();
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (word != null && word.Equals(c[i]))
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"Word found {count} time in the string");
+        }
+    }
+}
