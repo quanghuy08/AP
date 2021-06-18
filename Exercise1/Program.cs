e0d163c59ba41a2b170bbe6730613a4a7c26e32d
NguyenQuangHuy <huynqth2008064@fpt.edu.vn>
2021-06-18 14:42:31 +0700

    update

---
+++ Exercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 40;
+            person.Name = "Amar";
+            Console.WriteLine("Person deatails - Name = "+person.Name+", Age = "+person.Age);
+            Console.WriteLine("Person deatails (After incrementing age) - Name = "+person.Name+", Age = "+(person.Age+1));
+        }
+    }
+}
